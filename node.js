8c9d8aafad37860d5c6d984f10a2a6cc97a2f33f
Akhilarappura <itsmeakhilarappura@gmail.com>
2024-07-05 15:11:25 +0530

    first Commit

---
+++ node.js
@@ -0,0 +1,16 @@
+//check even or odd
+
+const express=require('express')
+const app=express();
+const port=3008
+
+app.get('/',(req,res)=>{
+    const num=req.query.num*1;
+    const result=num%2===0?'even':'odd'
+    res.send(`the num is${num} and result is ${result}`)
+})
+
+
+app.listen(port,()=>{
+    console.log("server is running");
+ })
